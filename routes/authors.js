bdd1f8872129f0213fe9a908b505a7c7811ce6e9
Isha <isha@gmail.com>
2024-09-10 14:57:07 +0530

    made a routes for author

---
+++ routes/authors.js
@@ -0,0 +1,23 @@
+import express from 'express'
+const router = express.Router()
+
+//all authors route
+
+router.get('/', (req, res)=>{
+    res.render('authors/index',{});
+
+});
+
+//new author route
+
+router.get('/new',(req, res)=>{
+    res.render('authors/new',{})
+});
+
+//create author route
+
+router.post('/', (req, res)=>{
+    res.send('Create')
+});
+
+export {router}
